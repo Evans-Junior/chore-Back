f95750406ea7bbfa0e46ecbf3e102662707dfef2
Evans-Junior <kwakukumi14@gmail.com>
2024-01-26 12:06:28 +0000

    started

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const mysql= require('mysql');
+const cors =require('cors');
+
+const app = express();
+app.use9(cors());
+
+const db=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'signup'
+});
